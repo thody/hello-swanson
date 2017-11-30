c1f636c58a12fbdcf03379571b27ae19e060db1e
Adam Thody <athody@heroku.com>
2017-11-30 14:23:43 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src="/swanson.jpg" alt="Ron Swanson" />
+      </div>
+    );
+  }
+}
+
+export default App;
